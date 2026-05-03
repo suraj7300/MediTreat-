ff964ab4babc01d702fe24ac0521b7659988145e
Suraj Sharma <surajluckey123@gmail.com>
2026-05-04 01:46:52 +0530

    Create Analytics.jsx

---
+++ frontend/src/pages/Analytics.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Analytics() {
+  return (
+    <div>
+      <h2>Analytics</h2>
+      <p>Clinic analytics and reports will appear here.</p>
+    </div>
+  );
+}
