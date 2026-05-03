ce02623b5977151d4f2d4969b3ccddbdcfffcfbf
Suraj Sharma <surajluckey123@gmail.com>
2026-05-04 01:45:15 +0530

    Create Topbar.jsx

---
+++ frontend/src/components/Topbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Topbar() {
+  return (
+    <div style={{ padding: 10, background: '#eee' }}>
+      <h3>MediTreat Dashboard</h3>
+    </div>
+  );
+}
